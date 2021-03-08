1a7e8dc4fa94f6b617b3d864872d078b057dd71e
DigitalHDR <digitalextremeultra@gmail.com>
2021-03-07 22:08:28 -0300

    v1

---
+++ src/componentes/BomDia.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (props) => [
+    <h1 key="1">Bom dia {props.nome}!</h1>,
+    <h2 key="1">Até breve!</h2>
+]
---
+++ src/componentes/Multiplos.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+// eslint-disable-next-line import/no-anonymous-default-export
+const BoaTarde = props => <h1>Boa tarde {props.nome}!</h1>
+const BoaNoite = props => <h1>Boa noite {props.nome}!</h1>
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export { BoaTarde, BoaNoite}
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {BoaTarde, BoaNoite}
